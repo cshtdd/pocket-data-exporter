2c5d3b2e9766cf2edd150ed158a9b6be4072fe63
Camilo Sanchez <camilin87@gmail.com>
2020-03-17 10:58:42 -0400

    Start getting a request token

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+require 'httparty'
+
 puts 'Export Pocket Articles'  
 
 consumer_key = ENV['CONSUMER_KEY'] || ''
@@ -7,5 +9,23 @@ if consumer_key.empty?
   puts 'ERROR: Consumer Key missing'
   exit 1
 end
+
+puts 'Retrieving auth token'
+
+token_response = HTTParty.post(
+  'https://getpocket.com/v3/oauth/request',
+  debug_output: STDOUT,
+  headers: {
+    'User-Agent': 'Pocket-Exporter',
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'X-Accept': 'application/x-www-form-urlencoded'
+  },
+  body: {
+    consumer_key: consumer_key,
+    redirect_uri: 'http://localhost:8080/auth'
+  }
+)
+
+puts token_response
 
 puts 'Export Completed'
