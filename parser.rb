f271d38f75de06aa33aa46bd8e931912ff78be18
Camilo Sanchez <camilin87@gmail.com>
2020-03-18 08:42:15 -0400

    Break down the articles per tag

--- parser.rb
+++ parser.rb
@@ -1,10 +1,40 @@
+require 'json'
+
 file_path = ARGV[0] || ''
 unless File.exist?(file_path)
   puts 'ERROR: File not found'
   exit 1
 end
 
-data = File.read(file_path)
+data_str = File.read(file_path)
 
-puts data.length
+data = JSON.parse(data_str)
+
+articles = data['list']
+
+articles_by_tag = {}
+
+articles.each do |id, info|
+  tag_info = info['tags'] || { 'untagged items': nil }
+
+  tag_info.keys.each do |tag|
+    unless articles_by_tag.include?(tag)
+      articles_by_tag[tag] = []
+    end
+
+    articles_by_tag[tag] << id
+  end
+
+  # puts tag_info
+end
+
+articles_by_tag.each do |tag, ids|
+  puts tag
+
+  ids.each do |id|
+    puts '   ' + articles[id]['resolved_url']
+  end
+end
+
+# puts articles
 
