fb78891a2640ea5bbd13417c43aa17b64f55bc2a
Camilo Sanchez <camilin87@gmail.com>
2020-03-18 15:26:50 -0400

    Create a downloader class to remove code out of the controllers

---
+++ lib/downloader.rb
@@ -0,0 +1,37 @@
+class Downloader
+  attr_accessor :pocket_api_client
+  attr_accessor :debug_enabled
+
+  def initialize(pocket_api_client, debug_enabled = false)
+    self.pocket_api_client = pocket_api_client
+    self.debug_enabled = debug_enabled
+  end
+
+  def read_access_token(request_token)
+    if request_token.empty?
+      return { error: true, message: 'Invalid Request Token' }
+    end
+
+    debug("Auth Request Code: #{request_token}")
+
+    access_token = pocket_api_client.read_access_token(request_token)
+
+    if access_token.empty?
+      return { error: true, message: 'Invalid Access Token' }
+    end
+
+    debug("Access Token: #{access_token}")
+
+    access_token
+  end
+
+  private
+
+  def debug(msg)
+    log(msg) if debug_enabled
+  end
+
+  def log(msg)
+    puts msg
+  end
+end
