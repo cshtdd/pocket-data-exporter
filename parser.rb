e42285d3fd345aa63d865e91624be9736ca5ae09
Camilo Sanchez <camilin87@gmail.com>
2020-03-18 14:10:26 -0400

    Extract a DataFormatter to support plaintext format

--- parser.rb
+++ parser.rb
@@ -1,4 +1,5 @@
 require './lib/pocket_parser'
+require './lib/data_formatter'
 
 file_path = ARGV[0] || ''
 unless File.exist?(file_path)
@@ -9,14 +10,5 @@ end
 data_str = File.read(file_path)
 articles_by_tag = PocketParser.articles_by_tag(data_str)
 
-total_count = articles_by_tag.values.flatten.uniq.count
-puts "Total Count: #{total_count}"
-
-
-articles_by_tag.each do |tag, urls|
-  puts tag
-
-  urls.each do |url|
-    puts '   ' + url
-  end
-end
+puts DataFormatter.unique_dict_values_plaintext(articles_by_tag)
+puts DataFormatter.dict_to_plaintext(articles_by_tag)
