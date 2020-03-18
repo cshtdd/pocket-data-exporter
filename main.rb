8c32baa9e7131e810cdc70dd7515fc3bd2420a1b
Camilo Sanchez <camilin87@gmail.com>
2020-03-18 14:15:19 -0400

    Export the tokens list by tag as plaintext

--- main.rb
+++ main.rb
@@ -3,6 +3,7 @@ require 'sinatra'
 require './lib/config'
 require './lib/pocket'
 require './lib/pocket_parser'
+require './lib/data_formatter'
 
 puts 'Initializing...'
 
@@ -100,6 +101,45 @@ get '/token_list_by_tags_json/:code' do
   end
 end
 
+get '/token_list_by_tags/:code' do
+  content_type 'text/html'
+  request_token = params[:code] || ''
+
+  if request_token.empty?
+    status 400
+    body 'Invalid Token'
+  else
+    puts "Auth Request Code: #{request_token}"
+
+    access_token = pocket_api.read_access_token(request_token)
+
+    if access_token.empty?
+      status 400
+      body 'Error Reading Access Token'
+    else
+      log_access_token_value = access_token
+      log_access_token_value = '*************' unless config.debug_enabled
+      puts "Access Token: #{log_access_token_value}"
+
+      articles_json = pocket_api.read_all_articles_json(access_token)
+
+      if articles_json.empty?
+        status 400
+        body 'Error Reading Articles'
+      end
+
+      articles_by_tag = PocketParser.articles_by_tag(articles_json)
+      response_body = ''
+      response_body << DataFormatter.unique_dict_values_plaintext(articles_by_tag)
+      response_body << "\r\n"
+      response_body << DataFormatter.dict_to_plaintext(articles_by_tag)
+
+      status 200
+      body response_body
+    end
+  end
+end
+
 puts 'Starting Export...'
-Launchy.open("#{config.server_url}/export/token_raw_data_json")
+Launchy.open("#{config.server_url}/export/token_list_by_tags")
 
