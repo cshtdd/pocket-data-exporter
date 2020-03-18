f29c5225f60cc6e50ceb88d6cae871e66253e8a6
Camilo Sanchez <camilin87@gmail.com>
2020-03-18 15:58:34 -0400

    Use the downloader for the data by tags

--- main.rb
+++ main.rb
@@ -72,37 +72,29 @@ get '/raw_data_json/:code' do
 end
 
 get '/list_by_tags_json/:code' do
-  content_type 'data:application/json'
   request_token = params[:code] || ''
+  access_token_info = downloader.read_access_token(request_token)
 
-  if request_token.empty?
-    status 400
-    body({ msg: 'Invalid Token' }.to_json)
+  if access_token_info[:error]
+    content_type 'application/json'
+    [400, { msg: access_token_info[:message] }.to_json]
   else
-    puts "Auth Request Code: #{request_token}"
+    access_token = access_token_info[:access_token]
+    log_access_token_value = access_token
+    log_access_token_value = '*************' unless config.debug_enabled
+    puts "Access Token: #{log_access_token_value}"
 
-    access_token = pocket_api.read_access_token(request_token)
+    articles_json = pocket_api.read_all_articles_json(access_token)
 
-    if access_token.empty?
-      status 400
-      body({ msg: 'Error Reading Access Token' }.to_json)
-    else
-      log_access_token_value = access_token
-      log_access_token_value = '*************' unless config.debug_enabled
-      puts "Access Token: #{log_access_token_value}"
-
-      articles_json = pocket_api.read_all_articles_json(access_token)
-
-      if articles_json.empty?
-        status 400
-        body({ msg: 'Error Reading Articles' }.to_json)
-      end
-
-      articles_by_tag = Pocket::Parser.articles_by_tag(articles_json)
-
-      status 200
-      body articles_by_tag.to_json
+    if articles_json.empty?
+      content_type 'application/json'
+      [400, { msg: 'Error Reading Articles' }.to_json]
     end
+
+    articles_by_tag = Pocket::Parser.articles_by_tag(articles_json)
+
+    content_type 'data:application/json'
+    body articles_by_tag.to_json
   end
 end
 
