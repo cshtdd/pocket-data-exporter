469eb54e93260bfc51ffd6d056355a15bb4fc6b9
Camilo Sanchez <camilin87@gmail.com>
2020-03-19 11:14:28 -0400

    Start building functionality to read archived or deleted items

--- lib/pocket/parser.rb
+++ lib/pocket/parser.rb
@@ -11,6 +11,24 @@ module Pocket
         .reject { |a| a['resolved_url'].nil? || a['resolved_url'].empty? }
     end
 
+    def self.archived_articles_with_url(data_str)
+      data = JSON.parse(data_str)
+      data['list']
+        .values
+        .filter { |a| a['status'] == '1' }
+        .filter { |a| a.include?('resolved_url') }
+        .reject { |a| a['resolved_url'].nil? || a['resolved_url'].empty? }
+    end
+
+    def self.deleted_articles_with_url(data_str)
+      data = JSON.parse(data_str)
+      data['list']
+        .values
+        .filter { |a| a['status'] == '2' }
+        .filter { |a| a.include?('resolved_url') }
+        .reject { |a| a['resolved_url'].nil? || a['resolved_url'].empty? }
+    end
+
     def self.urls_by_tag(articles)
       articles_by_tag = {}
 
